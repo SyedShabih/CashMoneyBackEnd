95b77b1fd143d2fa2be9c1ecd015739375aec9f9
Syed Shabih ul Hussain <shabih554@gmail.com>
2024-11-26 13:38:00 +0500

    -BACK TO ORIGINAL

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    port: process.env.PORT,
+    mongoURI: process.env.MONGO_URI,
+    botToken: process.env.BOT_TOKEN,
+    gameURL: process.env.GAME_URL,
+    domain: process.env.DOMAIN,
+    gameShortName: process.env.GAME_SHORT_NAME
+};
--- src/operations.js
+++
@@ -1,8 +0,0 @@
-export const registerUser = (telegramId, firstName, lastName, username) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const dummyUserId = "dummy_user_id";
-      resolve(dummyUserId);
-    }, 0);
-  });
-};
